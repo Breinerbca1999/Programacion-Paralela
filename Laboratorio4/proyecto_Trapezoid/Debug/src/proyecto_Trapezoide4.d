225254eddface2a5847033d336daf77e0562fe30
Breinerbca1999 <breinerbca2015@gmail.com>
2020-01-27 14:41:25 -0500

    Añadiendo Labo 4

---
+++ Laboratorio4/proyecto_Trapezoid/Debug/src/proyecto_Trapezoide4.d
@@ -0,0 +1,7 @@
+src/proyecto_Trapezoide4.o: ../src/proyecto_Trapezoide4.c \
+ /usr/lib/openmpi/include/mpi.h \
+ /usr/lib/openmpi/include/mpi_portable_platform.h
+
+/usr/lib/openmpi/include/mpi.h:
+
+/usr/lib/openmpi/include/mpi_portable_platform.h:
